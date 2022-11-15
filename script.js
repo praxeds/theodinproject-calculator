a144bd1335af3c030ed6484698846ab366f32e9e
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-14 22:38:08 -0300

    Changed a bunch of else if statements to a switch

--- script.js
+++ script.js
@@ -7,26 +7,12 @@ const startAudio = new Audio('assets/audio/start.mp3')
 const clickSound = new Audio('assets/audio/click.mp3')
 
 
-turnOnBtn.addEventListener('click', function (e) {
-    if (lcdDisplay.classList.contains('noDisplay') == true) {
-        lcdDisplay.classList.remove('noDisplay')
-        startAudio.play()
-    } else {
-        return
-    }
-})
 
 
-clearBtn.addEventListener('click', function (e) {
-    clearDisplay()
-})
-function clearDisplay() {
-    if (calcOutput.innerText !== '') {
-        calcOutput.innerText = ''
-    } else {
-        return
-    }
-}
+window.addEventListener('keydown', keyboardInput)
+turnOnBtn.addEventListener('click', turnOnDisplay)
+clearBtn.addEventListener('click', clearDisplay)
+calcOutput.addEventListener('transitionend', removeTransition)
 
 
 function add(a, b) {
@@ -46,26 +32,64 @@ function operate(operator, a, b) {
     return operator(a, b)
 }
 
-document.addEventListener('keydown', (key) => {
-    if (lcdDisplay.classList.contains('noDisplay') == false) {
-        if (key.key == 'Backspace') {
-            const calcOutputArr = calcOutput.innerHTML.split('')
-            calcOutputArr.pop()
-            calcOutput.innerHTML = calcOutputArr.join('')
-        }
+
+function turnOnDisplay() {
+    if (lcdDisplay.classList.contains('noDisplay') == true) {
+        lcdDisplay.classList.remove('noDisplay')
+        startAudio.play()
+    } else {
+        return
     }
+}
+
+function clearDisplay() {
+    if (calcOutput.innerText !== '') {
+        calcOutput.innerText = ''
+    } else {
+        return
+    }
+}
+
+function keyboardInput(key) {
+    if (lcdDisplay.classList.contains('noDisplay')) return
+
+    if (key.key == 'Backspace') {
+        const calcOutputArr = calcOutput.innerHTML.split('')
+        calcOutputArr.pop()
+        calcOutput.innerHTML = calcOutputArr.join('')
+    }
+
     if (calcOutput.innerText.length < 8) {
-        if (key.key == ' ') { return }
+        // if (key.key == ' ') return
         // if (!isNaN(Number(key.key)) || key.key == '/' || key.key == '*' || key.key == '-' || key.key == '+' || key.key == '.' || key.key == '=' || key.key == 'Enter' || key.key == 'Backspace') {
         // console.log(key.key)
         // }
-        if (!isNaN(Number(key.key))) {
-            calcOutput.innerHTML += key.key
-        }
-        if (key.key == '.') {
-            if (calcOutput.innerText.includes('.') == false && calcOutput.innerText.length > 0) {
+        // if (!isNaN(Number(key.key))) {
+        //     calcOutput.innerHTML += key.key
+        // }
+        // else if (key.key == '.') {
+        //     if (calcOutput.innerText.includes('.') == false && calcOutput.innerText.length > 0) {
+        //         calcOutput.innerHTML += key.key
+        //     }
+        // }
+
+        switch (true) {
+            case (key.key == ' '):
+                return
+            case (!isNaN(Number(key.key))):
                 calcOutput.innerHTML += key.key
-            }
+                break
+            case (key.key == '.'):
+                if (calcOutput.innerText.includes('.') == false && calcOutput.innerText.length > 0) {
+                    calcOutput.innerHTML += key.key
+                }
+                break
+
         }
     }
-})
+    calcOutput.classList.add('selected')
+}
+
+function removeTransition(e) {
+    this.classList.remove('selected')
+}
